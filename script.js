828f486ace60133ad68605ac2a3e22a1b40a304f
Michael <l1ghtdev.88@gmail.com>
2022-10-05 18:50:56 -0700

    Remove code that was refactored/no longer used

--- script.js
+++ script.js
@@ -48,26 +48,6 @@ document.querySelector('.check').addEventListener('click',  function() {
             document.querySelector('.score').textContent = 0;
         }
     }
-    // else if(guess > secretNumber) {
-    //     if(score > 1) {
-    //         document.querySelector('.message').textContent = 'Too High!';
-    //         score--;
-    //         document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message').textContent = 'You lost the game!'
-    //         document.querySelector('.score').textContent = 0;
-    //     }
-    // }
-    // else if(guess < secretNumber) {
-    //     if(score > 1) {
-
-    //         score--;
-    //         document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message').textContent = 'You lost the game!'
-    //         document.querySelector('.score').textContent = 0;
-    //     }
-    // }
 });
 
 
